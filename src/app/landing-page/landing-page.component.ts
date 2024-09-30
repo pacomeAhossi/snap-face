11b444c201e4aab93196f7f8bc15c06305364050
Pacome AHOSSI <pacomeahossi175@gmail.com>
2024-09-30 12:59:28 +0100

    Adding a template form

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
+  userEmail: string = '';
+
   constructor(private router: Router) {}
 
   onContinue() {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
   }
 }
