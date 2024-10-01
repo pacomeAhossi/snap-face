2848b1e5f8b786589a4dbb0fcb896196682771cc
Pacome AHOSSI <pacomeahossi175@gmail.com>
2024-10-01 15:27:37 +0100

    Validating forms

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { FaceSnap } from '../models/face-snap';
@@ -15,16 +20,27 @@ import { FaceSnap } from '../models/face-snap';
 export class NewFaceSnapComponent implements OnInit {
   snapForm!: FormGroup;
   faceSnapPreview$!: Observable<FaceSnap>;
+  urlRegex!: RegExp;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.snapForm = this.formBuilder.group({
-      title: [null],
-      description: [null],
-      imageUrl: [null],
-      location: [null],
-    });
+    this.urlRegex =
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+    this.snapForm = this.formBuilder.group(
+      {
+        title: [null, Validators.required],
+        description: [null, Validators.required],
+        imageUrl: [
+          null,
+          [Validators.required, Validators.pattern(this.urlRegex)],
+        ],
+        location: [null],
+      },
+      {
+        updateOn: 'blur',
+      }
+    );
 
     this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
       map((formValue) => ({
